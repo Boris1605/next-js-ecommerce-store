adbaf5ba3934827dc6cea7d34069d6fef65a2443
Boris Djuric <boris_djuric@hotmail.com>
2024-02-15 19:14:36 +0100

    Add logo image

--- app/page copy.tsx
+++ app/page copy.tsx
@@ -1,4 +1,4 @@
-// import Image from 'next/image';
+import Image from 'next/image';
 // import Button from './Button';
 // import styles from './page.module.scss';
 // import imageName from '../public/images/imageName.jpg'
@@ -6,6 +6,7 @@
 export default function Home() {
   return (
     <div>
+      <Image src="/logoimage/logo.webp" alt="Logo" width={50} height={50} />
       <h1>SMART GADGETS</h1>
     </div>
   );
