e4abc73c0ca2381a563b16aba5a60b6c1be1e27a
Boris Djuric <boris_djuric@hotmail.com>
2024-02-20 16:33:50 +0100

    Reinstall Prettier and ESLint

--- databaseQueries.sql
+++ databaseQueries.sql
@@ -1,20 +1,20 @@
 CREATE TABLE products (
-  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name varchar(50) NOT NULL,
-  type varchar(50) NOT NULL,
-  price numeric(3, 2) NOT NULL,
-  currency varchar(3) NOT NULL
+ id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+ name varchar(50) NOT NULL,
+ type varchar(50) NOT NULL,
+ price numeric(3, 2) NOT NULL,
+ currency varchar(3) NOT NULL
 );
 
 INSERT INTO products
-  (name, type, price, currency)
+ (name, type, price, currency)
 VALUES
-  ('Magsafe Powerbank', 'Battery', '9.99', 'EUR'),
-  ('Smart Ring', 'Ring', '4.99', 'EUR'),
-  ('Smart Thermostat', 'Thermostat', '1.99', 'EUR'),
-  ('Lamp with Speaker', 'Lamp', '7.99', 'EUR');
+ ('Magsafe Powerbank', 'Battery', '9.99', 'EUR'),
+ ('Smart Ring', 'Ring', '4.99', 'EUR'),
+ ('Smart Thermostat', 'Thermostat', '1.99', 'EUR'),
+ ('Lamp with Speaker', 'Lamp', '7.99', 'EUR');
 
-  SELECT * FROM products;
+ SELECT * FROM products;
 
 CREATE DATABASE ecommerce_migration;
 CREATE USER ecommerce_migration WITH ENCRYPTED PASSWORD 'ecommerce_migration';
