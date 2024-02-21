6d9bb801d446d3d45118d5ad105a874c516db9a3
Boris Djuric <boris_djuric@hotmail.com>
2024-02-21 16:06:29 +0100

    Delete comments

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -4,18 +4,18 @@ import { getProductInsecure } from '../../../database/products';
 import ProductQuantityForm from './ProductQuantityForm';
 import styles from './page.module.scss';
 
-export async function generateMetadata(props) {
-  const singleProduct = await getProductInsecure(props.params.productId);
-  return {
-    title: singleProduct?.name,
-  };
-}
-
 type Props = {
   params: {
     productId: number;
   };
 };
+
+export async function generateMetadata(props: Props) {
+  const singleProduct = await getProductInsecure(props.params.productId);
+  return {
+    title: singleProduct?.name,
+  };
+}
 
 // Defining the ProductPage component
 export default async function ProductPage(props: Props) {
@@ -35,15 +35,13 @@ export default async function ProductPage(props: Props) {
           <h1 className={styles.title}>Product: {singleProduct.name}</h1>
           <Image
             src={`/images/${singleProduct.id}.webp`}
-            // src={singleProduct?.image}
-            alt={singleProduct?.name}
+            alt={singleProduct.name}
             width={200}
             height={200}
             data-test-id="product-image"
           />
           <p data-test-id="product-price">{singleProduct.price}</p>
           <ProductQuantityForm
-            type="number"
             data-test-id="product-quantity"
             productId={singleProduct.id}
           />
